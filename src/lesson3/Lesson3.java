b08b7301a12817d45bfa69f07ed511c322f80300
narek <narekaveyan@gmail.com>
2021-09-06 21:30:37 +0400

    homework 3

---
+++ src/lesson3/Lesson3.java
@@ -0,0 +1,12 @@
+package lesson3;
+
+public class Lesson3 {
+    public static void main(String[] args) {
+        System.out.println();
+        if (args.length > 0) {
+            System.out.println("First arg is:" + args[0]);
+        } else {
+            System.out.println(" No Arguments");
+        }
+    }
+}
